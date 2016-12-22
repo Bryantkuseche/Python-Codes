06b2ffee31f97130c0ced52078d809c78720c8ee
Bryan Useche <bryan.useche.10@gmail.com>
2016-12-22 12:34:15 -0400

    Update Trackers Finder in text.py
    Adding Ping check, but isn't work

--- Trackers Finder in text.py
+++ Trackers Finder in text.py
@@ -1,4 +1,5 @@
-import re
+import re, os
+
 
 fname = raw_input("Enter file name: ")
 print "Searching Trackers..."
@@ -9,8 +10,6 @@ http = list()
 num_link_http = 0
 udp = list()
 num_link_udp = 0
-magnet = list()
-magnet_num = 0
 for link in data:
 	link = link.rstrip()
 	if re.search('http://.*' , link):
@@ -19,21 +18,28 @@ for link in data:
 	elif re.search('udp://.*' , link):
 		udp.append(link)
 		num_link_udp = num_link_udp + 1
-	elif re.search('magnet:.*', link):
-		magnet.append(link)
-		magnet_num = magnet_num + 1
-		
-
 for link in http:
-	print link
+	hostname = link
+	response = os.system("ping -c 1 " + hostname)
+	#and then check the response...
+	if response == 0:
+		print hostname, 'is up!'
+	else:
+		print hostname, 'is down!'
+
 
 for link in udp:
-	print link
+	hostname = link 
+	response = os.system("ping -c 1 " + hostname)
 
-for link in magnet:
-	print magnet
+	#and then check the response...
+	if response == 0:
+		print hostname, 'is up!'
+	else:
+		print hostname, 'is down!'
+
+
 
 print "Statistics:\n"
 print "There is" , num_link_http , "Http's links"
 print "There is" , num_link_udp ,  "Udp's links" 
-print "There is" , magnet_num , "Magnet's links"
