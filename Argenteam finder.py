12c0b68e43ebf21406f970c5fb772e4381b10013
Bryan Useche <bryan.useche.10@gmail.com>
2017-02-28 23:43:38 -0400

    Update Argenteam finder.py
    Writing the sample code

--- Argenteam finder.py
+++ Argenteam finder.py
@@ -1,9 +1,18 @@
-#Buscador de series y descargas por temporada
+import httplib, urllib, re
+from bs4 import BeautifulSoup
 
-import bs4 from BeautifulSoup
-import urllib
-import re
-
-#Obtencion de serie y temporada
+serie = raw_input("Introduzca el nombre de la serie: ")
+sitio = raw_input("BUscar en Rarbg o Argenteam?: ")
 
 
+parametros = urllib.urlencode({'search': serie })
+cabeceras = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+abrir_conexion = httplib.HTTPConnection(sitio +":80")
+abrir_conexion.request("POST", "/search", parametros, cabeceras)
+respuesta = abrir_conexion.getresponse()
+ver_source = respuesta.read()
+abrir_conexion.close()
+soup = BeautifulSoup(ver_source , 'html.parser')
+url = soup('a')
+for link in url:
+	print link
